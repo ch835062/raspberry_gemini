c5b25e43507a5784a542b76c75f0d60450f31ed1
ch835062 <ch835062@hotmail.com>
2026-03-30 11:22:05 +0800

    modify

--- test/main.py
+++ test/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from test!")
+from google import genai
+from dotenv import load_dotenv
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview",
+    contents="天空為什麼是藍的？"
+)
+
+print(response.text)
